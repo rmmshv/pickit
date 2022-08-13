c7b348268fa4dc833a27f39e4da27e375a13ac61
rimma <32555704+rmmshv@users.noreply.github.com>
2022-08-13 14:58:10 -0700

    Fix in 'business_id' function

--- pickit.py
+++ pickit.py
@@ -4,16 +4,32 @@
 import requests
 from yelpAPI import get_key
 
-business_id = 'WavvLdfdP6g8aZTtbBQHTw'
+API_HOST = 'https://api.yelp.com'
+BUSINESS_PATH = '/v3/businesses/'
+
 
 # Define API key, andpoint, and header
-API_KEY = get_key()
+API_KEY = 'ruKk2xMOzG09cGB6OUIQg48nJ_66MckLb1bKq_uZJb-4M_44H1za0AFJFgundmRR-k4xRhELjq0H24d-rqtke-kcI-Ne7pjPPI9ZSov9Ggoal_W4Q2MClS0ugeD2YnYx'
 ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
 HEADERS = {'Authorisation': "bearer %s" % API_KEY}
 
+# Define business id
+def get_business(business_id):
+    """ Query the business API by a business id
+
+        Args:
+            business_id (str): The id of the business to query
+        Returns:
+            dict: JSON responsefrom the request
+    """
+
+    business_path = BUSINESS_PATH + business_id
+
+    return request(API_HOST, business_path)
+
+
 # Ask the user for the rype of food
 food_type = input("Please pick an option: 1.Restaurant 2.Fast food 3.Dessert 4.Coffee 5.Boba 6.Alcohol ")
-
 
 # Define the parameters
 PARAMETERS = {'term': food_type,
