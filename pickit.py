22e27f6ab6e77555915156c42b982c11541584ca
rimma <32555704+rmmshv@users.noreply.github.com>
2022-08-16 19:41:29 -0700

    replaced API key, fixed output layout

--- pickit.py
+++ pickit.py
@@ -1,32 +1,14 @@
-# Yelp business search URL - 'https://api.yelp.com/v3/businesses/search'
-
-# Import the modules
 import requests
-from yelpAPI import get_key
+import json
+import yelpAPI
 
+business_id = '4AErMBEoNzbk7Q8g45kKaQ'
 API_HOST = 'https://api.yelp.com'
-BUSINESS_PATH = '/v3/businesses/'
 
-
-# Define API key, andpoint, and header
-API_KEY = 'ruKk2xMOzG09cGB6OUIQg48nJ_66MckLb1bKq_uZJb-4M_44H1za0AFJFgundmRR-k4xRhELjq0H24d-rqtke-kcI-Ne7pjPPI9ZSov9Ggoal_W4Q2MClS0ugeD2YnYx'
+# Define API key, endpoint, and header
+API_KEY = yelpAPI.api_key
 ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
-HEADERS = {'Authorisation': "bearer %s" % API_KEY}
-
-# Define business id
-def get_business(business_id):
-    """ Query the business API by a business id
-
-        Args:
-            business_id (str): The id of the business to query
-        Returns:
-            dict: JSON responsefrom the request
-    """
-
-    business_path = BUSINESS_PATH + business_id
-
-    return request(API_HOST, business_path)
-
+HEADERS = {'Authorization': 'bearer %s' % API_KEY}
 
 # Ask the user for the rype of food
 food_type = input("Please pick an option: 1.Restaurant 2.Fast food 3.Dessert 4.Coffee 5.Boba 6.Alcohol ")
@@ -43,5 +25,5 @@ response = requests.get(url = ENDPOINT, params = PARAMETERS, headers = HEADERS)
 # Convert JSON to a dictionary
 business_data = response.json()
 
-for bis in business_data['businesses']:
-    print(bis['name'])
+for biz in business_data['businesses']:
+    print(biz['name'])
